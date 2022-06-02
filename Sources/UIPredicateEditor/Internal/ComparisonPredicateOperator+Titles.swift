45fc0b27e9e764de0679bad1390a0703ee1bf082
Nikhil Nigade <nikhil@dezinezync.com>
2022-06-02 17:52:17 +0530

    `Operator` from localized title
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Sources/UIPredicateEditor/Internal/ComparisonPredicateOperator+Titles.swift
+++ Sources/UIPredicateEditor/Internal/ComparisonPredicateOperator+Titles.swift
@@ -16,6 +16,8 @@ extension NSCompoundPredicate.LogicalType {
       return NSLocalizedString("Any", comment: "Any")
     case .not:
       return NSLocalizedString("None", comment: "None")
+    @unknown default:
+      fatalError("unknown logical type")
     }
   }
 }
@@ -52,6 +54,39 @@ extension NSComparisonPredicate.Operator {
     case .between:
       return NSLocalizedString("between", comment: "between")
     @unknown default:
+      fatalError("Unknown operator")
+    }
+  }
+  
+  static func from(_ title: String) -> NSComparisonPredicate.Operator {
+    switch title {
+    case NSLocalizedString("less than", comment: "less than"):
+      return .lessThan
+    case NSLocalizedString("less than or equal to", comment: "less than or equal to"):
+      return .lessThanOrEqualTo
+    case NSLocalizedString("greater than", comment: "greater than"):
+      return .greaterThan
+    case NSLocalizedString("greater than or equal to", comment: "greater than or equal to"):
+      return .greaterThanOrEqualTo
+    case NSLocalizedString("equal to", comment: "equal to"):
+      return .equalTo
+    case NSLocalizedString("not equal to", comment: "not equal to"):
+      return .notEqualTo
+    case NSLocalizedString("matches", comment: "matches"):
+      return .matches
+    case NSLocalizedString("like", comment: "like"):
+      return .like
+    case NSLocalizedString("begins with", comment: "begins with"):
+      return .beginsWith
+    case NSLocalizedString("ends with", comment: "ends with"):
+      return .endsWith
+    case NSLocalizedString("in", comment: "in"):
+      return .in
+    case NSLocalizedString("contains", comment: "contains"):
+      return .contains
+    case NSLocalizedString("between", comment: "between"):
+      return .between
+    default:
       fatalError("Unknown operator")
     }
   }
