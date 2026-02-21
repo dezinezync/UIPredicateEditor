608b8b730724d9d3eeaadd6944137e9f84fe8907
Nikhil Nigade <nikhil@dezinezync.com>
2026-02-21 16:11:59 +0530

    formatting
    Signed-off-by: Nikhil Nigade <nikhil@dezinezync.com>

--- Package.swift
+++ Package.swift
@@ -4,27 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIPredicateEditor",
-    defaultLocalization: "en",
-    platforms: [.iOS(.v14), .macCatalyst(.v14)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "UIPredicateEditor",
-            targets: ["UIPredicateEditor"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "UIPredicateEditor",
-            dependencies: []),
-        .testTarget(
-            name: "UIPredicateEditorTests",
-            dependencies: ["UIPredicateEditor"]),
-    ]
+  name: "UIPredicateEditor",
+  defaultLocalization: "en",
+  platforms: [.iOS(.v14), .macCatalyst(.v14)],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "UIPredicateEditor",
+      targets: ["UIPredicateEditor"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "UIPredicateEditor",
+      dependencies: []),
+    .testTarget(
+      name: "UIPredicateEditorTests",
+      dependencies: ["UIPredicateEditor"]),
+  ]
 )
